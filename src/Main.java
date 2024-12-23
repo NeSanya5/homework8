71f3c54b9227162bcf02cc1f9a2d45d07f5c97e5
Шороховы <soroho78@gmail.com>
2024-12-24 02:21:13 +0300

    homework8.1

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,21 @@ public class Main {
         float[] arrayFloat = {1.57f, 7.654f, 9.986f};
         char[] arrayString = {'a', 'b', 'c'};
         System.out.println("Задача 2");
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < arrayInt.length; i++) {
             System.out.print(arrayInt[i]);
             if (i < 2) {
                 System.out.print(", ");
             }
         }
         System.out.println();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < arrayFloat.length; i++) {
             System.out.print(arrayFloat[i]);
             if (i < 2) {
                 System.out.print(", ");
             }
         }
         System.out.println();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < arrayString.length; i++) {
             System.out.print(arrayString[i]);
             if (i < 2){
                 System.out.print(", ");
@@ -29,21 +29,21 @@ public class Main {
         }
         System.out.println();
         System.out.println("Задача 3");
-        for (int i = 2; i != -1; i--) {
+        for (int i = 2; i != arrayInt.length - 4; i--) {
             System.out.print(arrayInt[i]);
             if (i != 0) {
                 System.out.print(", ");
             }
         }
         System.out.println();
-        for (int i = 2; i != -1; i--) {
+        for (int i = 2; i != arrayFloat.length - 4; i--) {
             System.out.print(arrayFloat[i]);
             if (i != 0) {
                 System.out.print(", ");
             }
         }
         System.out.println();
-        for (int i = 2; i != -1; i--) {
+        for (int i = 2; i != arrayString.length - 4; i--) {
             System.out.print(arrayString[i]);
             if (i != 0){
                 System.out.print(", ");
@@ -51,7 +51,7 @@ public class Main {
         }
         System.out.println();
         System.out.println("Задача 4");
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] % 2 != 0) {
                 arrayInt[i]++;
             }
